dcf0efb342053d0e9d5b6081d9d890d7a8afd881
kazi fahim <kazifahim661@gmail.com>
2024-04-26 16:16:18 +0600

    banner section responsive done

--- src/pages/AllSpots.jsx
+++ src/pages/AllSpots.jsx
@@ -1,24 +1,52 @@
 import { useEffect, useState } from "react";
 import Card from "./Card";
 
+
 const AllSpots = () => {
-    const [data, setData] = useState([])
+    const [datas, setData] = useState([])
     const [loader,setLoader]=useState(true)
     const [dataInUI,setDataInUi]=useState(false)
+    const [text,setText]=useState('all')
 
 
     useEffect(() => {
         fetch('http://localhost:5000/spots')
             .then(res => res.json())
-            .then(data => setData(data))
-    }, [])
+            .then(data => {
+                if(text==='all'){
+                    setData(data)
+                }
+                else if(text==='lower'){
+                    setData(data.sort((a, b) => a.average_cost - b.average_cost))
+                    
+                }
+                else if(text==='high'){
+                    setData(data.sort((a,b)=>b.average_cost - a.average_cost))
+                }
+                
+                
+
+            })
+            
+            
+                
+            
+
+
+
+    }, [text,datas])
+
+    
 
     setTimeout(()=>{
         setLoader(false)
         setDataInUi(true)
     },4000)
 
-
+    const handleClick=text=>{
+        console.log(text)
+        setText(text)
+    }
 
     return (
         <div className="" >
@@ -30,10 +58,22 @@ const AllSpots = () => {
             <div className={`flex justify-center items-center ${loader?'block':'hidden'} `}>
                 <span className="loading loading-bars loading-lg"></span>
                 </div>
+                {/* drop down */}
+            <div className="flex justify-center md:justify-end">
+                <details className="dropdown  w-56">
+                    <summary className="m-1 btn bg-[#e8604c] ">Sort by average_cost</summary>
+                    <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                        <li><a onClick={()=>handleClick('lower')} >Lower cost</a></li>
+                        <li><a onClick={()=>handleClick('high')}>Higher cost</a></li>
+                    </ul>
+                </details>
+
+
+                </div>
 
             <div className={`max-w-7xl pt-10 mx-auto gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ${dataInUI?'block':'hidden'}`}>
                 {
-                    data.map(spot => <Card key={spot._id} spot={spot}></Card>)
+                    datas.map(spot => <Card key={spot._id} spot={spot}></Card>)
                 }
             </div>  
 
